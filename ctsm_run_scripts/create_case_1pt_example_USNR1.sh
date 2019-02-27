f6131cf50440aef6668697bb2340b8240a30fee4
Shawn P. Serbin <sserbin@bnl.gov>
2019-02-27 12:00:45 -0700

    Updated ctsm_example_data/USNR1/USNR1_CTSM_Example_Data.tar.gz, added ctsm_run_scripts/create_case_1pt_example_USNR1.sh

--- ctsm_run_scripts/create_case_1pt_example_USNR1.sh
+++ ctsm_run_scripts/create_case_1pt_example_USNR1.sh
@@ -38,20 +38,33 @@ echo ${PWD}
 # Modifying : env_mach_pes.xml --- NEED TO REVIST THIS TO OPTIMIZE SETTINGS
 echo "*** Modifying xmls  ***"
 
+./xmlchange RUN_TYPE=STARTUP
+./xmlchange CALENDAR=GREGORIAN
 ./xmlchange --id DEBUG --val FALSE
 ./xmlchange --file env_run.xml --id PIO_DEBUG_LEVEL --val 0
-./xmlchange --id STOP_N --val 2
-./xmlchange --id RUN_STARTDATE --val '2000-01-01'
-./xmlchange --id STOP_OPTION --val ndays
-./xmlchange --id REST_N --val 2
-./xmlchange --id REST_OPTION --val ndays
-./xmlchange --id CLM_FORCE_COLDSTART --val off
+./xmlchange --id STOP_N --val 1
+./xmlchange --id RUN_STARTDATE --val '1998-01-01'
+./xmlchange --id STOP_OPTION --val nyears
+./xmlchange --id REST_N --val 0
+./xmlchange --id REST_OPTION --val nyears
+./xmlchange --id CLM_FORCE_COLDSTART --val on
 ./xmlchange --id RESUBMIT --val 0
 ./xmlchange --file env_run.xml --id DOUT_S_SAVE_INTERIM_RESTART_FILES --val FALSE
 ./xmlchange --file env_run.xml --id DOUT_S --val FALSE
 ./xmlchange --file env_run.xml --id DOUT_S_ROOT --val '$CASEROOT/run'
 ./xmlchange --file env_run.xml --id RUNDIR --val ${CASE_NAME}/run
 ./xmlchange --file env_build.xml --id EXEROOT --val ${CASE_NAME}/bld
+
+./xmlchange -a CLM_CONFIG_OPTS='-nofire'
+./xmlchange ATM_DOMAIN_FILE=domain.lnd.fv0.9x1.25_NR1.nc
+./xmlchange LND_DOMAIN_FILE=domain.lnd.fv0.9x1.25_NR1.nc
+./xmlchange ATM_DOMAIN_PATH=/ctsm_example_data/USNR1_CTSM_Example_Data/
+./xmlchange LND_DOMAIN_PATH=/ctsm_example_data/USNR1_CTSM_Example_Data/
+./xmlchange MOSART_MODE=NULL
+
+./xmlchange DOUT_S=FALSE
+./xmlchange DEBUG=FALSE
+./xmlchange INFO_DBUG=0
 
 # Optimize PE layout for run
 ./xmlchange NTASKS_ATM=1,ROOTPE_ATM=0
@@ -67,7 +80,7 @@ echo "*** Modifying xmls  ***"
 # Set run location to case dir
 ./xmlchange --file env_build.xml --id CIME_OUTPUT_ROOT --val ${CASE_NAME}
 # =======================================================================================
- 
+
 
 # =======================================================================================
 #./cesm_setup
@@ -75,11 +88,23 @@ echo "*** Running case.setup ***"
 ./case.setup
 
 cat >> user_nl_clm <<EOF
+fsurdat = '/ctsm_example_data/USNR1_CTSM_Example_Data/surfdata_0.9x1.25_NR1.nc'
 hist_empty_htapes = .true.
 hist_fincl1 = 'NEP','NPP','GPP','TOTECOSYSC','TOTVEGC','TLAI','EFLX_LH_TOT_R'
-hist_mfilt             = 24
+hist_mfilt             = 8760
 hist_nhtfrq            = -1
 EOF
+
+## define met params
+dtlimit  = 1.0e9, 1.0e9, 1.0e9
+streams = 'datm.streams.txt.CLMGSWP3v1.Solar 1998 1998 2010',
+          'datm.streams.txt.CLMGSWP3v1.Precip 1998 1998 2010',
+          'datm.streams.txt.CLMGSWP3v1.TPQW 1998 1998 2010',
+mapalgo = 'nn', 'nn', 'nn'
+
+## define stream files and edit
+cp /ctsm_example_data/USNR1_CTSM_Example_Data/user_datm.streams.txt.CLMGSWP3v1.* .
+
 
 echo *** Build case ***
 ./case.build
